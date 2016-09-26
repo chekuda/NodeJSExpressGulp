665397ee3a7bc5f42b0754cdcaa2aa0f1c06c682
chekuda <joselchecaexpo@gmail.com>
2016-09-26 12:27:11 +0100

    ejs templating added

--- app.js
+++ app.js
@@ -9,17 +9,15 @@ var port = process.env.PORT || 5000;
 //Set up the public directory as static directory
 //For this: 'use' will use first the directory added--middleware
 app.use(express.static('public'));
-//This is for seta variable views which is into ./src/views
+//This is for set a variable views which is into ./src/views
 app.set('views','./src/views');
-
-app.engine('.hbs',handlebars({extname: '.hbs'}));//this only for handlebars
 
 //The engine for views
 //Change to jade for different engine
-app.set('view engine','.hbs');
+app.set('view engine','ejs');
 
 app.get('/', function (req, res) {
-    res.render('index',{list:['1','2','3']});
+    res.render('index',{title: "Hello from render",list:['Books','Titles','Authors']});
 });
 
 app.get('/book', function (req, res) {
